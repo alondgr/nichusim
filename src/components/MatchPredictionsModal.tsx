c3800e2235f272588fd940fb0cdae1f3a0eee97f
Alon <alon.sela87@gmail.com>
2026-05-30 15:08:05 +0300

    Fix JSX syntax error in MatchPredictionsModal

--- src/components/MatchPredictionsModal.tsx
+++ src/components/MatchPredictionsModal.tsx
@@ -153,7 +153,6 @@ export default function MatchPredictionsModal({ isOpen, onClose, match, sport }:
                         </span>
                       </div>
                     )}
-                  </div>
                 </motion.div>
               ))
             )}
